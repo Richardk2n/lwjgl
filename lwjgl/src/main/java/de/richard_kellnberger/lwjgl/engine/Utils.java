5a4faf12da8e63b04e47cca66e4b9f4d88371ce6
Richardk2n <Richard1.Kellnberger@web.de>
2017-10-23 21:35:29 +0200

    triangle

---
+++ lwjgl/src/main/java/de/richard_kellnberger/lwjgl/engine/Utils.java
@@ -0,0 +1,17 @@
+package de.richard_kellnberger.lwjgl.engine;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Utils {
+
+    public static String loadResource(String fileName) throws Exception {
+        String result;
+        try (InputStream in = Utils.class.getResourceAsStream(fileName);
+                Scanner scanner = new Scanner(in, "UTF-8")) {
+            result = scanner.useDelimiter("\\A").next();
+        }
+        return result;
+    }
+
+}
---
+++ lwjgl/src/main/java/de/richard_kellnberger/lwjgl/game/Main.java
@@ -0,0 +1,19 @@
+package de.richard_kellnberger.lwjgl.game;
+
+import de.richard_kellnberger.lwjgl.engine.GameEngine;
+import de.richard_kellnberger.lwjgl.engine.IGameLogic;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.start();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
